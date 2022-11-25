255f9e06db64593e05fd232f3821bf426a142f1c
rediska97 <Kingstoonesis@gmail.com>
2022-11-25 11:25:29 +0200

    Create Clock app

---
+++ src/Clock.js
@@ -0,0 +1,13 @@
+import './Clock.css';
+
+function Clock() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Clock App
+      </header>
+    </div>
+  );
+}
+
+export default Clock;
